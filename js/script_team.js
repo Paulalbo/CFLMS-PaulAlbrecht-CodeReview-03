5f62a7bc5a65b1ee88d29d1d66bea8eb3d102d17
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-05-30 16:54:22 +0200

    final fixes

--- js/script_team.js
+++ js/script_team.js
@@ -1,10 +1,10 @@
 function showteam (){
 
   var teamMembers = [
-      {name:"Fran", email:"fran@index.com",image:"img/person1.jpg"},
-      {name:"Bill", email:"bill@index.com",image:"img/person2.jpg"},
-      {name:"Fritz", email:"fritz@index.com",image:"img/person3.jpg"},
-      {name:"Olivia", email:"olivia@index.com",image:"img/person4.jpg"}
+      {name:"Fran", email:"fran@index.com",image:"img/person1.jpg", personality:"crazy"},
+      {name:"Bill", email:"bill@index.com",image:"img/person2.jpg", personality:"happy"},
+      {name:"Fritz", email:"fritz@index.com",image:"img/person3.jpg", personality:"always tired"},
+      {name:"Olivia", email:"olivia@index.com",image:"img/person4.jpg", personality:"obsessed with the devil"}
   ];
 
 
@@ -14,11 +14,15 @@ for (var i = 0; i < teamMembers.length; i++) {
       var name = teamMembers[i].name;
       var email = teamMembers[i].email;
       var image = teamMembers[i].image;
+      var personality = teamMembers[i].personality;
 
       let containerstart = "<div class=\"containerteam\">"
       let containerend = "</p></div>"
 
-      container.innerHTML += containerstart + "<img src=\"" + image + "\"><br><p>" + "Name: " + name + "<br> E-mail: " + email + "<br>" + containerend;
-      
- }
+      container.innerHTML += containerstart + "<img src=\"" + image + "\"><br><p>" + "Name: " + name + "<br> E-mail: " + email + "<br>" + "<br> <div>" + personality +"</div>" + containerend;
 }
+      
+}
+
+
+
